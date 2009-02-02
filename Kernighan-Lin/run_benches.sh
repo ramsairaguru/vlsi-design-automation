a173230a3be557dafd0e45cc9755a571f0d6ceab
ferggo <ferggo@a62f42e4-ebfb-11dd-a550-c997509ed985>
2009-02-02 14:03:34 +0000

    Added a check to make sure that initial values of the adjacency matrix are zeroed.

--- Kernighan-Lin/run_benches.sh
+++ Kernighan-Lin/run_benches.sh
@@ -1,19 +1,19 @@
-(./kl benchmarks/bench_11.net > results/bench_11.result) >& results/bench_11.output
+(time ./kl benchmarks/bench_11.net > results/bench_11.result) >& results/bench_11.output &
 
-(./kl benchmarks/bench_12.net > results/bench_12.result) >& results/bench_12.output
+(time ./kl benchmarks/bench_12.net > results/bench_12.result) >& results/bench_12.output &
 
-(./kl benchmarks/bench_13.net > results/bench_13.result) >& results/bench_13.output
+(time ./kl benchmarks/bench_13.net > results/bench_13.result) >& results/bench_13.output &
 
-(./kl benchmarks/bench_14.net > results/bench_14.result) >& results/bench_14.output
+(time ./kl benchmarks/bench_14.net > results/bench_14.result) >& results/bench_14.output &
 
-(./kl benchmarks/bench_16.net > results/bench_16.result) >& results/bench_16.output
+(time ./kl benchmarks/bench_16.net > results/bench_16.result) >& results/bench_16.output &
 
-(./kl benchmarks/bench_17.net > results/bench_17.result) >& results/bench_17.output
+(time ./kl benchmarks/bench_17.net > results/bench_17.result) >& results/bench_17.output &
 
-(./kl benchmarks/bench_18.net > results/bench_18.result) >& results/bench_18.output
+(time ./kl benchmarks/bench_18.net > results/bench_18.result) >& results/bench_18.output &
 
-(./kl benchmarks/bench_22.net > results/bench_22.result) >& results/bench_22.output
+(time ./kl benchmarks/bench_22.net > results/bench_22.result) >& results/bench_22.output &
 
-(./kl benchmarks/bench_23.net > results/bench_23.result) >& results/bench_23.output
+(time ./kl benchmarks/bench_23.net > results/bench_23.result) >& results/bench_23.output &
 
-(./kl benchmarks/bench_26.net > results/bench_26.result) >& results/bench_26.output
+(time ./kl benchmarks/bench_26.net > results/bench_26.result) >& results/bench_26.output &
