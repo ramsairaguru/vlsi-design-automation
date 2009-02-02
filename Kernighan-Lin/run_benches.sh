eae184ffda74abb2ab4df3ad5e8c2ed69cc6bbcd
ferggo <ferggo@a62f42e4-ebfb-11dd-a550-c997509ed985>
2009-02-02 13:32:14 +0000

    Changed the data structure of the adjacentcy matrix to be a vector of maps instead of one big map, so that searching is constant + a small log instead of just one big log.

--- Kernighan-Lin/run_benches.sh
+++ Kernighan-Lin/run_benches.sh
@@ -1,20 +1,19 @@
-echo Working on Bench 11
 (./kl benchmarks/bench_11.net > results/bench_11.result) >& results/bench_11.output
-echo Working on Bench 12
+
 (./kl benchmarks/bench_12.net > results/bench_12.result) >& results/bench_12.output
-echo Working on Bench 13
+
 (./kl benchmarks/bench_13.net > results/bench_13.result) >& results/bench_13.output
-echo Working on Bench 14
+
 (./kl benchmarks/bench_14.net > results/bench_14.result) >& results/bench_14.output
-echo Working on Bench 16
+
 (./kl benchmarks/bench_16.net > results/bench_16.result) >& results/bench_16.output
-echo Working on Bench 17
+
 (./kl benchmarks/bench_17.net > results/bench_17.result) >& results/bench_17.output
-echo Working on Bench 18
+
 (./kl benchmarks/bench_18.net > results/bench_18.result) >& results/bench_18.output
-echo Working on Bench 22
+
 (./kl benchmarks/bench_22.net > results/bench_22.result) >& results/bench_22.output
-echo Working on Bench 23
+
 (./kl benchmarks/bench_23.net > results/bench_23.result) >& results/bench_23.output
-echo Working on Bench 26
+
 (./kl benchmarks/bench_26.net > results/bench_26.result) >& results/bench_26.output
